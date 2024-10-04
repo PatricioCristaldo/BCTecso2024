a1713911852cf37edb385fe5ff73d1444b603ee7
Fer Cruz <cruzfernandomartin94@gmail.com>
2024-10-04 19:14:07 -0300

    login - Login basic layout started.

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -3,14 +3,15 @@ import * as Yup from "yup";
 import { Form, Button, Container } from "react-bootstrap";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import mumaLogo from '../assets/isologo.png'
 
 const validationSchema = Yup.object().shape({
   email: Yup.string("Debe ingresar su usuario")
     .email("Debe ingresar un email")
     .required("Usuario es requerido"),
   password: Yup.string()
-    .matches(/^\d+$/, "El password debe ser numérico")
-    .required("Password es requerido"),
+    // .matches(/^\d+$/, "El password debe ser numérico")
+    // .required("Password es requerido"),
 });
 
 function Login() {
@@ -33,16 +34,18 @@ function Login() {
   };
 
   return (
-    <div className="bg-dark vh-100 justify-content-center align-items-center">
+    <div className="vh-100 justify-content-center align-items-center">
       <Container
-        className="bg-dark text-white rounded-2 py-4 align-self-center"
+        className=" rounded-2 py-4 align-self-center"
         style={{
           maxWidth: "400px",
           paddingTop: "50px",
           boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
         }}
       >
-        <h2>Login</h2>
+        <div className="pt-5 pb-5 d-flex justify-content-center">
+          <img src={mumaLogo} alt="Muma isologo"/>
+        </div>
         <Formik
           initialValues={{ email: "", password: "" }}
           validationSchema={validationSchema}
@@ -62,11 +65,11 @@ function Login() {
           }) => (
             <Form onSubmit={formikHandleSubmit}>
               <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
+                {/* <Form.Label>Email address</Form.Label> */}
                 <Form.Control
-                  type="emial"
+                  type="email"
                   name="email"
-                  placeholder="Ingrese su usuario"
+                  placeholder="Email*"
                   value={values.email}
                   onChange={handleChange}
                   onBlur={handleBlur}
@@ -78,11 +81,11 @@ function Login() {
               </Form.Group>
 
               <Form.Group controlId="formBasicPassword" className="mt-3">
-                <Form.Label>Password</Form.Label>
+                {/* <Form.Label>Password</Form.Label> */}
                 <Form.Control
                   type="password"
                   name="password"
-                  placeholder="Ingrese su password"
+                  placeholder="Contraseña*"
                   value={values.password}
                   onChange={handleChange}
                   onBlur={handleBlur}
@@ -92,15 +95,26 @@ function Login() {
                   {errors.password}
                 </Form.Control.Feedback>
               </Form.Group>
-
-              <div className="justify-content-center w-100">
+              <div className="d-flex flex-row justify-content-between">
+                <p>Recordarme</p>
+                <p>Olvidaste tu Contraseña?</p>
+              </div>
+              <div className="justify-content-center w-100 mt-5">
                 <Button
                   variant="primary"
                   type="submit"
-                  className="mt-4 align-self-center"
+                  className="mt-2 align-self-center w-100 py-2"
                   disabled={isSubmitting}
                 >
                   Login
+                </Button>
+                <Button
+                  variant="primary"
+                  type="submit"
+                  className="mt-2 align-self-center w-100 py-2"
+                  disabled={isSubmitting}
+                >
+                  Crear cuenta
                 </Button>
               </div>
             </Form>
