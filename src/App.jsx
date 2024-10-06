31c75e561d0d4e2ff44433d927a034565ca451d3
Fer Cruz <cruzfernandomartin94@gmail.com>
2024-10-06 01:33:04 -0300

    login - Login form styled and functional. To-do: splash screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
-import Login from './Pages/Login';
+import Login from './Pages/login/Login';
+import RecoverPassword from './Pages/RecoverPassword';
+import './app.css'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/home" element={<Home />} />
+      <Route path="/recoverpassword" element={<RecoverPassword />} />
     </Routes>
   );
 }
--- src/Pages/login/Login.jsx
+++ src/Pages/login/Login.jsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import { Formik } from "formik";
 import * as Yup from "yup";
 import { Form, Button, Container } from "react-bootstrap";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
-import mumaLogo from '../assets/isologo.png'
+import { useNavigate, Link } from "react-router-dom";
+import './login.css'
+import mumaLogo from '../../assets/isologo.png'
 
 const validationSchema = Yup.object().shape({
   email: Yup.string("Debe ingresar su usuario")
@@ -14,7 +16,19 @@ const validationSchema = Yup.object().shape({
     // .required("Password es requerido"),
 });
 
+
 function Login() {
+  const [rememberMe, setRememberMe] = useState(false);
+  const handleRememberMe = () => {
+    setRememberMe (prevRememberMe => !prevRememberMe);
+  }
+
+  function rememberLogin( token ){
+    if (rememberMe){
+      localStorage.setItem('accessToken', token)
+    }
+}
+
   const navigate = useNavigate();
   const handleSubmit = (values) => {
     console.log("Email:", values.email);
@@ -26,6 +40,7 @@ function Login() {
       })
       .then((response) => {
         console.log("Response:", response.data?.token);
+        rememberLogin( response.data?.token )
         navigate("/home");
       })
       .catch((error) => {
@@ -36,15 +51,10 @@ function Login() {
   return (
     <div className="vh-100 justify-content-center align-items-center">
       <Container
-        className=" rounded-2 py-4 align-self-center"
-        style={{
-          maxWidth: "400px",
-          paddingTop: "50px",
-          boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
-        }}
+        className=" rounded-2 py-4 mt-5 align-self-center login"
       >
         <div className="pt-5 pb-5 d-flex justify-content-center">
-          <img src={mumaLogo} alt="Muma isologo"/>
+          <img src={mumaLogo} alt="Muma isologo" />
         </div>
         <Formik
           initialValues={{ email: "", password: "" }}
@@ -74,6 +84,7 @@ function Login() {
                   onChange={handleChange}
                   onBlur={handleBlur}
                   isInvalid={!!errors.email && touched.email}
+                  className="py-2 neutro-input"
                 />
                 <Form.Control.Feedback type="invalid">
                   {errors.email}
@@ -81,7 +92,6 @@ function Login() {
               </Form.Group>
 
               <Form.Group controlId="formBasicPassword" className="mt-3">
-                {/* <Form.Label>Password</Form.Label> */}
                 <Form.Control
                   type="password"
                   name="password"
@@ -90,28 +100,38 @@ function Login() {
                   onChange={handleChange}
                   onBlur={handleBlur}
                   isInvalid={!!errors.password && touched.password}
+                  className="py-2 neutro-input"
                 />
                 <Form.Control.Feedback type="invalid">
                   {errors.password}
                 </Form.Control.Feedback>
               </Form.Group>
-              <div className="d-flex flex-row justify-content-between">
-                <p>Recordarme</p>
-                <p>Olvidaste tu Contraseña?</p>
+              <div className="d-flex flex-row justify-content-between mt-3">
+                <Form.Group controlId="formBasicCheckbox">
+                  <Form.Check 
+                    type="checkbox" 
+                    label="Recordarme" 
+                    name="rememberMe" 
+                    className="fs-7"
+                    checked={rememberMe}
+                    onChange={handleRememberMe}
+                  />
+                </Form.Group>
+                <Link to="/recoverpassword" className="fs-8 recover-password">¿Olvidaste tu Contraseña?</Link>
               </div>
               <div className="justify-content-center w-100 mt-5">
                 <Button
                   variant="primary"
                   type="submit"
-                  className="mt-2 align-self-center w-100 py-2"
+                  className="mt-2 align-self-center w-100 custom-primary-btn"
                   disabled={isSubmitting}
                 >
                   Login
                 </Button>
                 <Button
-                  variant="primary"
+                  variant="primary-outline"
                   type="submit"
-                  className="mt-2 align-self-center w-100 py-2"
+                  className="mt-2 align-self-center w-100 custom-secondary-btn"
                   disabled={isSubmitting}
                 >
                   Crear cuenta
